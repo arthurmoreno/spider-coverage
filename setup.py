147f818c9aa252d6308c85021ecd0367baf7b5c8
Arthur <arthurmoreno_6@hotmail.com>
2017-11-21 16:52:13 -0300

    Adiciona arquivos iniciais do coverage.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='spidercoverage',
+    version='0.1',
+    description='Set utils for spider coverage.',
+    author='Arthur Moreno',
+    author_email='arthur.moreno@intelivix.com',
+    keywords=['spider', 'coverage'],
+    packages=['spidercoverage'],
+    )
